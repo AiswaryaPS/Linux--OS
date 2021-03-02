da6914bbdff551ddb6980cc8b7e0a8bcdd041963
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-03-02 08:52:55 +0530

    Update test.c

--- test.c
+++ test.c
@@ -2,17 +2,4 @@
 #include "myutils.c"
 #include "bitmask.c"
 #include <stdio.h>
-int main() {
-  char s[]="Aiswarya";
-  int prime=5,palindrome=151,fact=4,num=5,n=1;
-  printf("length of the string is %lu\n ",mystrlen(s));
-  if(isPrime(prime))
-      printf(" Number is prime\n");
-  if(ispalindrome(palindrome))
-      printf("Number is palindrome\n");
-  printf("factorial of %d is %d \n",fact,factorial(fact));
-  printf("setting %d th of %d is %d \n",n,num,set(num,n));
-  printf("setting %d th of %d is %d \n",n,num,clear(num,n));
-  printf("setting %d th of %d is %d \n",n,num,flip(num,n));
-return 0;
-}
+int main() 
