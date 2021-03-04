c0ce3d749b6962d700d4c8694806dc4d24ba6ed4
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-03-04 21:15:44 +0530

    update

---
+++ Assignment2/Processes/prgm2/execl.h
@@ -0,0 +1,20 @@
+#ifndef __EXECLP_H
+#define __EXECLP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//fork
+#include <unistd.h> 
+
+
+//process
+#include <sys/types.h> 
+
+//wait
+#include <sys/wait.h>
+
+void toCompile();
+
+#endif
