d6113b7510ab59b9872b6084d2a3a91ab3e1a92f
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-03-05 22:16:59 +0530

    update

--- Assignment3/mutex_circular.c
+++ Assignment3/mutex_circular.c
@@ -1,13 +1,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdlib.h>
-#include <stdio.h>
-
-/*
-This program provides a possible solution for producer-consumer problem using mutex and semaphore.
-I have used 5 producers and 5 consumers to demonstrate the solution. You can always play with these values.
-*/
-
+#include <stdio.h> 
 #define MaxItems 5 // Maximum items a producer can produce or a consumer can consume
 #define BufferSize 5 // Size of the buffer
 
