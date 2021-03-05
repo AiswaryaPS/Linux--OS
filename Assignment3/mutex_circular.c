e2ac2f017a93e1196806bb72a8d30d177ee23d6f
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-03-05 22:13:22 +0530

    update

--- Assignment3/mutex_circular.c
+++ Assignment3/mutex_circular.c
@@ -2,52 +2,51 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define MaxItems  
-#define BufferSize 5
+
+/*
+This program provides a possible solution for producer-consumer problem using mutex and semaphore.
+I have used 5 producers and 5 consumers to demonstrate the solution. You can always play with these values.
+*/
+
+#define MaxItems 5 // Maximum items a producer can produce or a consumer can consume
+#define BufferSize 5 // Size of the buffer
 
 int in = 0;
 int out = 0;
-int item=0;
 int buffer[BufferSize];
 pthread_mutex_t mutex;
 
 void *producer(void *pno)
 {   
-    if(in==BufferSize-1)
-    {
-        printf("producer stack is full \n");
-    }
-    else
-    {
-     for(int i = 0; i < MaxItems; i++) {
+    int item;
+    for(int i = 0; i < MaxItems; i++) {
         item = rand(); // Produce a random item
         pthread_mutex_lock(&mutex);
+        /* wait for space in buffer */
+        while (((in + 1) % BufferSize) == out)
+        {
         /* put value item into the buffer */
-        buffer[in] = item;
-        printf("Producer %d: Insert Item %d at %d\n", *((int *)pno),buffer[in],in);
-        in = (in + 1) % BufferSize;     
+           buffer[in] = item;
+           printf("Producer %d: Insert Item %d at %d\n", *((int *)pno),buffer[in],in);
+           in = (in + 1) % BufferSize;     
+        }
         pthread_mutex_unlock(&mutex);
-     }
     }
 }
 void *consumer(void *cno)
 {   
-    if(out==BufferSize-1)
-    {
-        printf("consumer stack is full\n");
-    }
-    
-    else
-    {
-     int item=0;
-     for(int i = 0; i < MaxItems; i++) {
+    int item=0;
+    for(int i = 0; i < MaxItems; i++) {
         pthread_mutex_lock(&mutex);
+        /* wait for buffer to fill */
+        while (in == out) 
+        {
         /* take one unit of data from the buffer */
-        item = buffer[out];
-        printf("Consumer %d: Remove Item %d from %d\n",*((int *)cno),item, out); 
-        out = (out + 1) % BufferSize;     
+           item = buffer[out];
+           printf("Consumer %d: Remove Item %d from %d\n",*((int *)cno),item, out); 
+           out = (out + 1) % BufferSize;     
+        }
         pthread_mutex_unlock(&mutex);
-     }
     }
 }
 
@@ -74,6 +73,6 @@ int main()
     }
 
     pthread_mutex_destroy(&mutex);
-
+    
     return 0;
 }
